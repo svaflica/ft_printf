1dc1d9b19921acb425ad409ef1669096c053199c
Alina Tarskya <djeanna@at-m1.21-school.ru>
2019-04-24 09:00:48 +0300

    First commit

---
+++ inc/ft_printf.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: djeanna <djeanna@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/04/24 08:39:30 by djeanna           #+#    #+#             */
+/*   Updated: 2019/04/24 08:42:51 by djeanna          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+#define FT_PRINTF_H
+
+# include <stdarg.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+void		ft_printf(char *str, ...);
+
+#endif
