07584f929d819027a75f99482a63297f0bdbfe09
Quellon humble Clubfoot karl <qclubfoo@vo-l6.21-school.ru>
2019-04-26 12:31:47 +0300

    ft_o complete

--- src/ft_type/ft_o.c
+++ src/ft_type/ft_o.c
@@ -6,7 +6,7 @@
 /*   By: qclubfoo <qclubfoo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/25 14:09:26 by qclubfoo          #+#    #+#             */
-/*   Updated: 2019/04/25 19:02:15 by qclubfoo         ###   ########.fr       */
+/*   Updated: 2019/04/26 11:20:17 by qclubfoo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,15 +21,29 @@ int	ft_o(t_param list, unsigned int i)
 	size = 0;
 	str = ft_itoa_base(i, 8);
 	tmp = ft_strlen(str);
-	if (list.minus == 0 && list.zero == 0 && list.width - list.hasht > tmp)
-		size = ft_print_symb(' ', list.width - list.hasht - tmp);
-	if (list.minus == 0 && list.zero == 1 && list.width - list.hasht > tmp)
-		size = ft_print_symb('0', list.width - list.hasht - tmp);
-	size += ft_print_symb('0', list.hasht);
-	size += ft_strlen(str);
-	ft_putstr(str);
-	if (list.minus == 1 && list.width - list.hasht > tmp)
-		size += ft_print_symb(' ', list.width - list.hasht - tmp);
+	if (list.precision == -1)
+	{
+		if (list.minus == 0 && list.zero == 0 && list.width - list.hasht > tmp)
+			size = ft_print_symb(' ', list.width - list.hasht - tmp);
+		if (list.minus == 0 && list.zero == 1 && list.width - list.hasht > tmp)
+			size = ft_print_symb('0', list.width - list.hasht - tmp);
+		size += ft_print_symb('0', list.hasht);
+		size += ft_strlen(str);
+		ft_putstr(str);
+		if (list.minus == 1 && list.width - list.hasht > tmp)
+			size += ft_print_symb(' ', list.width - list.hasht - tmp);
+	}
+	else
+	{
+		if (list.precision > tmp)
+			size += ft_print_symb('0', list.precision - tmp - list.hasht);
+		size += ft_print_symb('0', list.hasht);
+		list.precision != 0 ? size += ft_strlen(str) : 0;
+		list.precision == 0 ? NULL : ft_putstr(str);
+		if (list.width > (list.precision + tmp))
+			size += ft_print_symb(' ', list.width - list.precision);
+	}
 	free(str);
 	return (size);
 }
+
