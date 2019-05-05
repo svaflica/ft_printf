97e3da30d801dc727528ddd666067e2202fa6fb7
Quellon humble Clubfoot karl <qclubfoo@mi-f2.21-school.ru>
2019-05-05 14:47:44 +0300

    just for merge

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: qclubfoo <qclubfoo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/24 09:23:26 by djeanna           #+#    #+#             */
-/*   Updated: 2019/05/01 16:18:43 by qclubfoo         ###   ########.fr       */
+/*   Updated: 2019/05/05 14:38:07 by qclubfoo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,25 +15,10 @@
 //gcc main.c ./src/*.c ./src/ft_param/*.c ./src/ft_str/*.c ./src/ft_type/*.c && ./a.out
 int main (void)
 {
-	// int a = -9999;
-	// char *str;
+	char *str = "some string";
+	long long i = 9223372036854775807;
 
-	// printf("%x\n", a);
-	// ft_printf("%x\n", a);
-
-	// printf("-->%x<--\n", -9999);
-	// printf("-->%-020x<--\n", -9999);
-	// printf("-->%#x<--\n", -9999);
-	// printf("-->%-x<--\n", -9999);
-	// printf("-->%#-30.20x<--\n", -9999);
-	// printf("-->%015x<--\n", -9999);
-	// printf("-->%015.x<--\n", -9999);
-	// ft_printf("-->%#10x<--\n", 0);
-	printf("%#.x\n", 0);
-	ft_printf("%.x\n", 0);
-	// ft_printf("-->%#010x<--\n", 0);
-	// printf("-->%-10x<--\n", -9999);
-	// printf("-->%10x<--\n", -9999);
+	printf("%20c", str[0]);
 
 
 }
