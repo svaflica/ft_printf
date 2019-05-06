2c3386e103d757ada1166c59be46c74ab35e2b65
Quellon humble Clubfoot karl <qclubfoo@mi-p5.21-school.ru>
2019-05-06 10:52:05 +0300

    fixed ft_c (print spaces instead of zeros)

--- src/ft_type/ft_c.c
+++ src/ft_type/ft_c.c
@@ -6,11 +6,7 @@
 /*   By: qclubfoo <qclubfoo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/25 10:52:41 by djeanna           #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2019/05/05 17:20:24 by qclubfoo         ###   ########.fr       */
-=======
-/*   Updated: 2019/05/05 20:18:05 by djeanna          ###   ########.fr       */
->>>>>>> origin/djeanna
+/*   Updated: 2019/05/06 10:49:30 by qclubfoo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,5 +25,5 @@ void	ft_c(t_param list, unsigned long long c, t_buf *buf)
 	ft_buf_add_c(buf, c);
 	if (list.minus == 1 && list.width > 1)
 		ft_buf_add_s(buf,
-			ft_memset(ft_strnew(list.width - 1), '0', list.width - 1));
+			ft_memset(ft_strnew(list.width - 1), ' ', list.width - 1));
 }
