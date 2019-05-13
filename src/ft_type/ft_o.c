79512d541df6fe37471b5907f35f585e4a54fa6f
Quellon humble Clubfoot karl <qclubfoo@mi-p5.21-school.ru>
2019-05-13 16:47:17 +0300

    fix some bugs in ft_o

--- src/ft_type/ft_o.c
+++ src/ft_type/ft_o.c
@@ -6,7 +6,7 @@
 /*   By: qclubfoo <qclubfoo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/25 14:09:26 by qclubfoo          #+#    #+#             */
-/*   Updated: 2019/05/09 13:30:14 by qclubfoo         ###   ########.fr       */
+/*   Updated: 2019/05/13 16:46:34 by qclubfoo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,13 @@ static void			prec_neg(char *str, t_param list, t_buf *buf)
 	int tmp;
 
 	tmp = ft_strlen(str);
-	*str == '0' && tmp == 1 ? tmp = 0 : 0;
-	if (list.minus == 0 && list.zero == 0 && list.width - list.hasht > tmp)
-		ft_buf_add_s(buf, ft_memnew(list.width - list.hasht - tmp, ' '), 0);
-	if (list.minus == 0 && list.zero == 1 && list.width - list.hasht > tmp)
+	// *str == '0' && tmp == 1 ? tmp = 0 : 0;
+	if (list.minus == 0 && list.zero == 1 && list.width - list.hasht > tmp && list.precision <= -1)
 		ft_buf_add_s(buf, ft_memnew(list.width - list.hasht - tmp, '0'), 0);
-	list.hasht == 1 && tmp != 0 ? ft_buf_add_c(buf, '0') : NULL;
+	else if (list.minus == 0 && list.zero == 0 && list.width - list.hasht > tmp)
+		ft_buf_add_s(buf, ft_memnew(list.width - list.hasht - tmp, ' '), 0);
+
+	list.hasht == 1 && *str != '0'/* tmp != 0 */ ? ft_buf_add_c(buf, '0') : NULL;
 	ft_buf_add_s(buf, str, 0);
 	if (list.minus == 1 && list.width - list.hasht > tmp)
 		ft_buf_add_s(buf, ft_memnew(list.width - list.hasht - tmp, ' '), 0);
@@ -36,10 +37,11 @@ static void			prec_pos(char *str, t_param list, t_buf *buf)
 	*str == '0' && tmp == 1 ? tmp = 0 : 0;
 	if (list.minus == 0)
 		{
-		if (list.width > list.precision && list.width > tmp && list.zero == 0)
-			ft_buf_add_s(buf, ft_memnew(list.width - (list.precision > tmp ? list.precision : tmp) - (list.hasht == 1 && list.precision < tmp), ' '), 0);
-		if (list.width > list.precision && list.width > tmp && list.zero == 1)
+		if (list.width > list.precision && list.width > tmp && list.zero == 1 && list.precision <= -1)
 			ft_buf_add_s(buf, ft_memnew(list.width - (list.precision > tmp ? list.precision : tmp) - (list.hasht == 1 && list.precision < tmp), '0'), 0);
+		else if (list.width > list.precision && list.width > tmp)
+			ft_buf_add_s(buf, ft_memnew(list.width - (list.precision > tmp ? list.precision : tmp) - (list.hasht == 1 && list.precision < tmp), ' '), 0);
+
 		if ((tmp > list.precision && list.hasht == 1) || (tmp == 0 && list.hasht == 1))
 			ft_buf_add_c(buf, '0');
 		if (list.precision > tmp)
@@ -77,7 +79,7 @@ void				ft_o(t_param list, va_list ap, t_buf *buf)
 				ft_itoa_base_ll((unsigned long)va_arg(ap, int), 8), buf);
 	else if (list.length == 'l' + 'l')
 		ft_type_o(list,
-				ft_itoa_base_ll((unsigned long long)va_arg(ap, int), 8), buf);
+				ft_itoa_base_ll((unsigned long long)va_arg(ap, long long), 8), buf);
 	else if (list.length == 'h')
 		ft_type_o(list,
 				ft_itoa_base_ll((unsigned short)va_arg(ap, int), 8), buf);
