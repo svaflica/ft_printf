1437c160e3d3bb1b162e823f6a85811a39555cd7
Quellon humble Clubfoot karl <qclubfoo@mi-p5.21-school.ru>
2019-05-16 11:37:51 +0300

    normed ft_o_null.c

--- src/ft_type/ft_o_null.c
+++ src/ft_type/ft_o_null.c
@@ -6,7 +6,7 @@
 /*   By: qclubfoo <qclubfoo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/14 12:58:34 by qclubfoo          #+#    #+#             */
-/*   Updated: 2019/05/14 14:42:22 by qclubfoo         ###   ########.fr       */
+/*   Updated: 2019/05/16 11:37:09 by qclubfoo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,10 +33,12 @@ void			prec_pos_null(char *str, t_param list, t_buf *buf)
 	if (list.minus == 0)
 	{
 		list.precision > -1 ? list.zero = 0 : 0;
-		if (list.width > list.precision/*  && list.precision > 0 */ && list.zero == 0)
-			ft_buf_add_s(buf, ft_memnew(list.width - (list.hasht == 1 && list.precision < 1) - list.precision, ' '), 0);
-		else if (list.width > list.precision/*  && list.precision > 0 */ && list.zero == 1)
-			ft_buf_add_s(buf, ft_memnew(list.width - list.hasht - list.precision, '0'), 0);
+		if (list.width > list.precision && list.zero == 0)
+			ft_buf_add_s(buf, ft_memnew(list.width - (list.hasht == 1 &&
+							list.precision < 1) - list.precision, ' '), 0);
+		else if (list.width > list.precision && list.zero == 1)
+			ft_buf_add_s(buf, ft_memnew(list.width - list.hasht -
+											list.precision, '0'), 0);
 		if (list.hasht == 1 && list.precision < 1)
 			ft_buf_add_c(buf, '0');
 		if (list.precision > 0)
@@ -48,7 +50,8 @@ void			prec_pos_null(char *str, t_param list, t_buf *buf)
 			ft_buf_add_c(buf, '0');
 		if (list.precision > 0)
 			ft_buf_add_s(buf, ft_memnew(list.precision, '0'), 0);
-		if (list.width > list.precision/*  && list.precision > 0 */ && list.zero == 0)
-			ft_buf_add_s(buf, ft_memnew(list.width - (list.hasht == 1 && list.precision < 1) - list.precision, ' '), 0);
+		if (list.width > list.precision && list.zero == 0)
+			ft_buf_add_s(buf, ft_memnew(list.width - (list.hasht == 1 &&
+							list.precision < 1) - list.precision, ' '), 0);
 	}
 }
